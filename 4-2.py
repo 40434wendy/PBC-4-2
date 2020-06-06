bfd095b725a18604cfba8cd3113015d7d976f2c7
Lydia <40434wendy@gmail.com>
2020-06-06 14:50:32 +0800

    三間消防局 ver

--- 4-2.py
+++ 4-2.py
@@ -11,7 +11,7 @@ for j in range(city):
     distance_list.append(distance)
 
     option=0
-    for i in range(city):
+    for i in range(city):            #第一間:計算最短總路徑長
         times[i]=int(times[i])
         distance[i]=int(distance[i])
         accumulated=times[i]*distance[i]
@@ -20,11 +20,22 @@ for j in range(city):
 
 least_distance=min(option_list)
 first_firestation=option_list.index(least_distance)
-builded_list.append(first_firestation)
+builded_list.append(firestation)
 
-for distance in distance_list:
+
+for distance in distance_list:       #第二間:從第一間的距離list中抓最遠的
     if distance[first_firestation]==0:
         selected=distance
         second_firestation=selected.index(max(selected))
-        selected[first_firestation]=9999
         builded_list.append(second_firestation)
+
+next_list=[]
+for distance in distance_list:
+    df=distance[first_firestation]
+    ds=distance[second_firestation]
+    next=min(df,ds)
+    next_list.append(next)
+
+third_firestation=next_list.index(max(next_list))
+
+print(first_firestation,second_firestation,third_firestation)
