f0ede5a06de9290076c063d0d68c5247d7152ca3
Lydia <40434wendy@gmail.com>
2020-06-06 21:54:24 +0800

    完成ver.1

--- 4-2.py
+++ 4-2.py
@@ -20,22 +20,43 @@ for j in range(city):
 
 least_distance=min(option_list)
 first_firestation=option_list.index(least_distance)
-builded_list.append(firestation)
+builded_list.append(first_firestation)
 
 
 for distance in distance_list:       #第二間:從第一間的距離list中抓最遠的
     if distance[first_firestation]==0:
         selected=distance
         second_firestation=selected.index(max(selected))
-        builded_list.append(second_firestation)
+builded_list.append(second_firestation)
 
+compare_list=[]
 next_list=[]
-for distance in distance_list:
-    df=distance[first_firestation]
-    ds=distance[second_firestation]
-    next=min(df,ds)
-    next_list.append(next)
+for m in range(firestation-2):      #接下去蓋
+    for distance in distance_list:
+        for k in builded_list:
+            compare_list.append(distance[k])
+        next=min(compare_list)
+        next_list.append(next)
+        compare_list=[]
+    next_firestation=next_list.index(max(next_list))
+    builded_list.append(next_firestation)
+    next_list=[]
 
-third_firestation=next_list.index(max(next_list))
+pick_distance=[]
+min_list=[]
+for distance in distance_list:     #總距離
+    for p in builded_list:
+        pick_distance.append(distance[p])
+    min_distance=min(pick_distance)
+    min_list.append(min_distance)
+    pick_distance=[]
 
-print(first_firestation,second_firestation,third_firestation)
+total=0
+for n in range(city):
+    total+=times[n]*min_list[n]
+
+for q in range(len(builded_list)):
+    builded_list[q]=str(builded_list[q]+1)
+
+format=",".join(builded_list)
+print(format+";"+str(total))
